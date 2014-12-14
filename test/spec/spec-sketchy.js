f14cab132ebb1a76d46128a893ff83993f10ac19
Gion Kunz <gion.kunz@gmail.com>
2014-12-14 23:53:23 +0100

    Initial commit

---
+++ test/spec/spec-sketchy.js
@@ -0,0 +1,15 @@
+describe('ctSketchy', function () {
+  'use strict';
+
+  beforeEach(function () {
+
+  });
+
+  afterEach(function () {
+
+  });
+
+  it('should be defined in chartist', function () {
+    expect(window.Chartist.plugins.ctSketchy).toBeDefined();
+  });
+});
